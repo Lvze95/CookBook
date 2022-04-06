92e8f4b005c1844904b4b4190f6bfaae294f488e
Jovana Stevanovic <jovanas111@gmail.com>
2022-04-06 10:53:04 +0200

    New Settings, CollectionView Fixed

--- CookBook.Android/Resources/Resource.designer.cs
+++ CookBook.Android/Resources/Resource.designer.cs
@@ -16688,49 +16688,52 @@ namespace CookBook.Droid
 			public const int settings = 2131165334;
 			
 			// aapt resource value: 0x7F070097
-			public const int slider = 2131165335;
+			public const int settingslight = 2131165335;
 			
 			// aapt resource value: 0x7F070098
-			public const int sliders = 2131165336;
+			public const int slider = 2131165336;
 			
 			// aapt resource value: 0x7F070099
-			public const int step5_1 = 2131165337;
+			public const int sliders = 2131165337;
 			
 			// aapt resource value: 0x7F07009A
-			public const int step5_2 = 2131165338;
+			public const int step5_1 = 2131165338;
 			
 			// aapt resource value: 0x7F07009B
-			public const int step5_3 = 2131165339;
+			public const int step5_2 = 2131165339;
 			
 			// aapt resource value: 0x7F07009C
-			public const int step5_4 = 2131165340;
+			public const int step5_3 = 2131165340;
 			
 			// aapt resource value: 0x7F07009D
-			public const int test_custom_background = 2131165341;
+			public const int step5_4 = 2131165341;
 			
 			// aapt resource value: 0x7F07009E
-			public const int thumbnail1 = 2131165342;
+			public const int test_custom_background = 2131165342;
 			
 			// aapt resource value: 0x7F07009F
-			public const int thumbnail2 = 2131165343;
+			public const int thumbnail1 = 2131165343;
 			
 			// aapt resource value: 0x7F0700A0
-			public const int thumbnail3 = 2131165344;
+			public const int thumbnail2 = 2131165344;
 			
 			// aapt resource value: 0x7F0700A1
-			public const int thumbnail4 = 2131165345;
+			public const int thumbnail3 = 2131165345;
 			
 			// aapt resource value: 0x7F0700A2
-			public const int thumbnail5 = 2131165346;
+			public const int thumbnail4 = 2131165346;
 			
 			// aapt resource value: 0x7F0700A3
-			public const int tooltip_frame_dark = 2131165347;
+			public const int thumbnail5 = 2131165347;
 			
 			// aapt resource value: 0x7F0700A4
-			public const int tooltip_frame_light = 2131165348;
+			public const int tooltip_frame_dark = 2131165348;
 			
 			// aapt resource value: 0x7F0700A5
-			public const int vecera = 2131165349;
+			public const int tooltip_frame_light = 2131165349;
+			
+			// aapt resource value: 0x7F0700A6
+			public const int vecera = 2131165350;
 			
 			static Drawable()
 			{
