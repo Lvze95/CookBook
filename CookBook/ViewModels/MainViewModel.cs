37b1bb93b69e3a8e402e94e99b4c938132bab994
Gamiraude <gamiraude@gmail.com>
2022-04-08 23:59:33 +0200

    LoadRecipes comments

--- CookBook/ViewModels/MainViewModel.cs
+++ CookBook/ViewModels/MainViewModel.cs
@@ -26,7 +26,30 @@ namespace CookBook.ViewModels
             _navigationService = navigation;
             _cookBookRepository = recipeRepository;
 
+            LoadRecipes();
+
             OpenListOfMeals = new Command<string>(OnSelectedOpenListOfMeals);
+        }
+
+        public void LoadRecipes()
+        {
+            var mainViewModel = new List<MainViewModel>();
+            var categories = _cookBookRepository.GetAllRecipeCategories();
+
+           /* foreach (var category in categories)
+            {
+                mainViewModel.Add(new MainViewModel())
+            }
+
+            var notesViewModel = new List<NoteItemViewModel>();
+            var notes = _notesRepository.GetAllNotes();
+
+            foreach (var note in notes)
+            {
+                notesViewModel.Add(new NoteItemViewModel(note));
+            }
+
+            NotesSource = new ObservableCollection<NoteItemViewModel>(notesViewModel); */
         }
 
         private void OnSelectedOpenListOfMeals(string obj)
